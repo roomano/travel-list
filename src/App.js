623499fe85c371a69eb2e314f970675e7facdddd
Romano Ivan Pedro <romanoivanpedro@gmail.com>
2023-11-03 10:52:00 +0200

    using derived state
    using derived state to calculate some listing stats

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Status />
+      <Status items={items} />
     </>
   );
 }
@@ -115,10 +115,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Status() {
+function Status({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀🚀🚀</em>
+      </p>
+    );
+  }
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  let percentage = 0;
+  // if (numPacked !== 0) {
+  //   percentage = Math.round((numPacked / numItems) * 100);
+  // }
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you a already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everthing!!! Ready to go ... ✈️✈️✈️"
+          : `🧳 You have ${numItems} items on your list, and you a already packed ${numPacked} items, at ${percentage}%`}
+      </em>
     </footer>
   );
 }
